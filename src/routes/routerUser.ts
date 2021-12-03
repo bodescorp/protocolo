9cb06e0798831656c21ffd9d361e6965cc4d578a
Glaymar A de França <glaymar2010@live.com>
2021-12-03 10:21:09 -0300

    Implementação de Listagem de Users

--- src/routes/routerUser.ts
+++ src/routes/routerUser.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "../controllers/User/AuthenticateUserController";
 import { CreateUserController } from "../controllers/User/CreateUserContoller";
+import { ListUserController } from "../controllers/User/ListUserController";
 import { ensureAdm } from "../middlewares/ensureAdm";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
@@ -8,8 +9,10 @@ const routerUser = Router();
 
 const createUserController = new CreateUserController();
 const authenticateUserController = new AuthenticateUserController();
+const listUserController = new ListUserController();
 
 routerUser.post("/users", ensureAuthenticated, ensureAdm, createUserController.handle)
 routerUser.post("/login", authenticateUserController.handle);
+routerUser.get("/users", ensureAuthenticated, ensureAdm, listUserController.handle)
 
 export { routerUser }
---
+++ src/services/User/ListUserService.ts
@@ -0,0 +1,18 @@
+import { getCustomRepository } from "typeorm"
+import { UsersRepositories } from "../../repositories/UsersRepositories";
+
+
+
+class ListUserService {
+    async execute() {
+        const userRepositories = getCustomRepository(UsersRepositories)
+
+        const users = userRepositories.find();
+
+        return users;
+    }
+}
+export { ListUserService }
+
+
+
